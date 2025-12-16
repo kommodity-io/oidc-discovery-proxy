03b713bd3fdc086c01bffaaea1cbaff54a3b26f6
Steffen Karlsson <steffen.karlsson@gmail.com>
2025-12-16 11:07:24 +0100

    fix(handler): remove unused code
    Signed-off-by: Steffen Karlsson <steffen.karlsson@gmail.com>

--- internal/handler/errors.go
+++
@@ -1,8 +0,0 @@
-package handler
-
-import "errors"
-
-var (
-	// ErrFailedToParseCABundle is returned when the serviceaccount CA bundle cannot be parsed.
-	ErrFailedToParseCABundle = errors.New("failed to parse serviceaccount CA bundle")
-)
